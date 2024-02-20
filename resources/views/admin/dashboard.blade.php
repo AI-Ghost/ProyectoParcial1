c80bea959c1fb93604ae7c7ac899bd1e4708e080
Diana <dvromero@uees.edu.ec>
2024-02-20 00:03:57 -0500

    commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Welcome!! ^_^</h1>
+    </div>
+
+    <div class="container mb-4">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Category
+            <h5>Display | Edit | Delete</h5>
+        </li>
+        <li class="list-group-item">Question
+            <h5>Display | Edit | Delete</h5>
+        </li>
+        <li class="list-group-item">Option
+            <h5>Display | Edit | Delete</h5>
+        </li>
+        <li class="list-group-item">Result
+            <h5>Display | View | Edit | Delete</h5>
+        </li>
+    </ul>
+    </div>
+
+    <div class="container">
+    <h5>Text Referenced:</h5>
+    <h9>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus rutrum arcu id hendrerit. Proin quis hendrerit risus. Quisque ullamcorper ante in nisl laoreet, id cursus purus auctor. Sed pretium odio sit amet ex sollicitudin rutrum. Pellentesque sit amet lacus mauris. Mauris ut sem vel ipsum sodales placerat. Integer vulputate varius ante vel ultrices. Maecenas molestie sem nec tortor ullamcorper posuere. Curabitur aliquet lobortis sem id dapibus. Donec ultrices sapien at dolor facilisis, non imperdiet leo auctor. Nulla aliquet purus odio, vitae vestibulum sem posuere eu. Morbi nulla dui, mollis sed pellentesque ut, convallis a justo. Proin ut massa elit. In lorem nulla, dictum dapibus rhoncus in, porttitor nec augue. Cras ultricies imperdiet urna et dapibus. Nam nec enim dui. Duis aliquam tristique suscipit. Aliquam id auctor eros. Sed vitae eleifend nulla, sed pharetra quam. Proin ut nulla suscipit, pretium metus tempus, convallis purus. Aenean tempus neque nunc, at vulputate mi ornare non. Suspendisse eget tristique lacus. Suspendisse quis turpis aliquet, accumsan dolor id, condimentum leo. Maecenas viverra elementum nibh non hendrerit. Cras tristique purus ac eros laoreet, pellentesque ornare eros viverra. Nulla facilisi. Nam ornare ligula neque, a fringilla tortor porta tempus. Mauris aliquam sit amet quam id tincidunt. Duis mattis arcu eu nunc porta cursus. Fusce cursus nunc sit amet nisi scelerisque, non dictum eros elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h9>
+    </div>
+    
+<!-- Content Row -->
+    
+
+    <!-- Content Row -->
+
+</div>
+@endsection
